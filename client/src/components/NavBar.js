b555079881fe1e763a691888aa2db7ba279ec3e7
Angela Donati <angeladonati93@gmail.com>
2022-09-04 14:56:18 -0400

    adjust navbar for mobile

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -6,40 +6,40 @@ import Auth from '../utils/auth';
 function NavBar() {
   return (
     <>
-      <Navbar bg="dark" variant="dark" expand="lg">
+      <Navbar bg="dark" variant="dark" expand="sm">
         <Container>
           <Navbar.Brand as={Link} to="/">
             kernel
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           {Auth.loggedIn() ? (
-                    <Navbar.Collapse id="basic-navbar-nav">
-          <Nav>
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav>
 
-            <Nav.Link as={Link} to="/dailytracker">
-              Daily Tracker
-            </Nav.Link>
-            <Nav.Link as={Link} to="/grid">
-              Grid
-            </Nav.Link>
-            <Nav.Link as={Link} to="/resources">
-              Resources
-            </Nav.Link>
-            <Nav.Link as={Link} to="/wordsofencouragement">
-              Kernels of Encouragement
-            </Nav.Link>
-            <Nav.Link eventKey={2} onClick={Auth.logout}>
-              Logout
-            </Nav.Link>
-          </Nav>
-          </Navbar.Collapse>
+                <Nav.Link as={Link} to="/dailytracker">
+                  Daily Tracker
+                </Nav.Link>
+                <Nav.Link as={Link} to="/grid">
+                  Grid
+                </Nav.Link>
+                <Nav.Link as={Link} to="/resources">
+                  Resources
+                </Nav.Link>
+                <Nav.Link as={Link} to="/wordsofencouragement">
+                  Kernels of Encouragement
+                </Nav.Link>
+                <Nav.Link eventKey={2} onClick={Auth.logout}>
+                  Logout
+                </Nav.Link>
+              </Nav>
+            </Navbar.Collapse>
 
-          ): <Nav.Link></Nav.Link> 
+          ) : <Nav.Link></Nav.Link>
 
           }
         </Container>
       </Navbar>
-      <br />
+      {/* <br /> */}
     </>
   );
 }

059c8f824af3e73d3525e652f1e2e155cb295d5b
Angela Donati <angeladonati93@gmail.com>
2022-09-05 13:41:36 -0400

    change connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost:27017/kerneldb",
+  process.env.MONGODB_URI || "mongodb://localhost:27017/kernel_db",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
